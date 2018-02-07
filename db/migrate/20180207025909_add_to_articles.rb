2bf5ffed9987658291e9dbfd792b237c1542f226
Vish Phadnis <VishPhadnis@hotmail.com>
2018-02-06 21:34:37 -0600

    recreated db migration files

--- db/migrate/20180207025909_add_to_articles.rb
+++ db/migrate/20180207025909_add_to_articles.rb
@@ -1,6 +1,5 @@
-class AddIsbnToArticles < ActiveRecord::Migration[5.1]
+class AddToArticles < ActiveRecord::Migration[5.1]
   def change
-    add_column :articles, :isbn, :string
     add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetime
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202031632) do
+ActiveRecord::Schema.define(version: 20180207031104) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.string "isbn"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

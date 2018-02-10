5c36584d1f7689590d27a576db3370a7c54a8b56
Vish Phadnis <VishPhadnis@hotmail.com>
2018-02-10 07:41:06 -0600

    Restrictions to pages and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
+# Prevent CSRF attacks by raising an exception.
+# For APIs, you may want to use :null_session instead
   protect_from_forgery with: :exception
+
+
+  # The following statement makes these two methods available to our views.
+  #By default, while these methods are available to other controllers, they're
+  #not accessible to the views
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
 
   resources:articles
 
-  get 'signup', to: "users#new"
+  get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  # route for log-in
+  get 'login', to: 'sessions#new'
+  # route for post to login
+  post 'login', to: 'sessions#create'
+  #route to log route - sessions controller, destroy action
+  delete 'logout', to: 'sessions#destroy'
 end
